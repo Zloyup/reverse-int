54a33e0c1d76f2e641b4718ec7051de60b606b25
Andrei Harelau <Dinotop4et@gmail.com>
2022-08-13 00:06:39 +0300

    feat: add index js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    let strng;
+
+    if (n < 0) {
+        n *= -1;
+        strng = n.toString(10);
+        return strng.split('').reverse().join('');
+    } else {
+        strng = n.toString(10);
+        return strng.split('').reverse().join('');;
+    };
+
 }
